615f3df1e092397f4439de5cbccc815f4b1b90e2
izwaxrai <raixhee@gmail.com>
2024-10-17 10:07:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🍟 Welcome to BurgerThing 🍔")
+st.header("Order Here ↓")
